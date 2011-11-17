db29e896a46f4d60896721cc7f70cbb1a8a91b9a
Christian Koerner <christian.koerner@gmail.com>
2011-11-17 15:13:27 +0100

    fixed classification experiments renamed script

--- machine_learning/classification_experiments.py
+++ machine_learning/classification_experiments.py
@@ -13,16 +13,18 @@ from sklearn import cross_validation
 #X, y = load_svmlight_file("/Users/chris/Dropbox/Experiment/SVM_Experiments/learn_discipline/data_to_learn_discipline_k=5")
 
 X, y = load_svmlight_file(
-     "/Users/chris/Dropbox/Experiment/SVM_Experiments/learn_discipline/data_to_learn_discipline_k=5")
+     "/Users/chris/Dropbox/Experiment/SVM_Experiments/learn_academic_status/specialist_svm")
 
-skf = cross_validation.StratifiedKFold(y, 10)
-
-
-
-clf = svm.sparse.SVC()
+skf = cross_validation.StratifiedKFold(y, 10, indices=True)
 
 
 
+clf = svm.sparse.SVC(kernel = 'linear')
+
+
+precisions = []
+recalls = []
+f1scores = []
 
 for train_index, test_index in skf:
     X_train, X_test = X[train_index], X[test_index]
@@ -35,8 +37,10 @@ for train_index, test_index in skf:
 
     prediction = clf.predict(X_test)
 
+    f1scores.append(metrics.f1_score(prediction, y_test))
+    precisions.append(metrics.precision_score(prediction, y_test))
+    recalls.append(metrics.recall_score(prediction, y_test))
 
-    print "test shape %s" % (y_test.shape)
-    print "prediction shape %s" % (prediction.shape)
-
-    print metrics.f1_score(prediction, y_test)
+print "avg f1 score: %s" % (sum(f1scores)/ len(f1scores))
+print "avg precision score: %s" % (sum(precisions)/ len(f1scores))
+print "avg recall score: %s" % (sum(recalls)/ len(f1scores))
